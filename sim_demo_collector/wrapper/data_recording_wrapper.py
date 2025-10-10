af4371eba353cad65704c3e3ff262f89f4b64e68
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-10 19:17:07 -0400

    Improve DataRecordingWrapper class

--- sim_demo_collector/wrapper/data_recording_wrapper.py
+++ sim_demo_collector/wrapper/data_recording_wrapper.py
@@ -25,53 +25,71 @@ class DataRecordingWrapper(gym.Wrapper):
             elif key.endswith("mask"):
                 dtype = bool
             self.root.create_dataset(
-                key,
+                name=key,
                 shape=(0, *shape),
                 chunks=(10, *shape),
                 dtype=dtype
             )
         self.root.create_dataset(
-            'action',
+            name='action',
             shape=(0, *self.shape_meta['action']),
             chunks=(10, *self.shape_meta['action']),
             dtype=np.float32
         )
-        self.is_done = False
-        self.data_buffer = {key: [] for key in self.obs_keys + ['action']}
+        self.root.create_dataset(
+            name='meta/episode_ends',
+            shape=(0,),
+            chunks=(10,),
+            dtype=np.int64
+        )
+
+        self._is_done = False
+        self._data_buffer = {key: [] for key in self.obs_keys + ['action']}
 
     def reset(self, *args, **kwargs) -> Dict:
-        if self.is_done:
+        if self._is_done:
             # Remove the last observations to align with action sequences
             for key in self.obs_keys:
-                self.data_buffer[key].pop()
-            self.save()
-            self.clear_buffer()
+                self._data_buffer[key].pop()
+            self._save()
+        self._clear_buffer()
+        self._is_done = False
         obs = super().reset(*args, **kwargs)
         for key in self.obs_keys:
-            self.data_buffer[key].append(obs[key])
+            self._data_buffer[key].append(obs[key])
         return obs
     
     def step(self, action: np.ndarray) -> Tuple:
-        self.data_buffer['action'].append(action)
+        self._data_buffer['action'].append(action)
         outputs = super().step(action)
         obs = outputs[0]
         for key in self.obs_keys:
-            self.data_buffer[key].append(obs[key])
-        self.is_done = outputs[2]
+            self._data_buffer[key].append(obs[key])
+        self._is_done = outputs[2]
         return outputs
 
-    def save(self) -> None:
+    def render(self) -> None:
+        return super().render()
+
+    def _save(self) -> None:
         for key in self.obs_keys + ['action']:
-            dataset = self.root[key]
-            data = np.stack(self.data_buffer[key])
-            start = dataset.shape[0]
-            end = start + data.shape[0]
-            dataset.resize(end, *data.shape[1:])
-            dataset[start: end] = data
+            data = self.root[key]
+            new_data = np.stack(self._data_buffer[key])
+            start = data.shape[0]
+            end = start + new_data.shape[0]
+            data.resize(end, *data.shape[1:])
+            data[start: end] = new_data
+
+        episode_ends = self.root['meta/episode_ends']
+        episode_len = np.array(len(self._data_buffer['action']))
+        last_end = episode_ends[-1] if episode_ends.shape[0] > 0 else 0
+        new_end = last_end + episode_len
+        episode_ends.resize(episode_ends.shape[0] + 1)
+        episode_ends[-1] = new_end
         
-    def clear_buffer(self) -> None:
-        for key in self.data_buffer.keys():
-            self.data_buffer[key].clear()
+    def _clear_buffer(self) -> None:
+        for key in self._data_buffer.keys():
+            self._data_buffer[key].clear()
 
 
 def test():
@@ -86,7 +104,7 @@ def test():
         use_object_obs=True,
         use_image_obs=True,
         use_depth_obs=True,
-        use_pcd_obs=True,
+        use_pc_obs=True,
         use_mask_obs=True,
     )
     env = DataRecordingWrapper(
@@ -95,17 +113,19 @@ def test():
             'obs': {
                 'frontview_image': [84, 84, 3],
                 'robot0_eye_in_hand_image_mask': [84, 84, 1],
+                'frontview_pc': [512, 3],
                 "robot0_eef_pos": [3]
             },
             'action': [7]
         },
         "test.zarr"
     )
-    obs = env.reset()
-    for i in range(5):
-        obs, rew, done, info = env.step(np.random.rand(7))
-    env.is_done = True
     env.reset()
-
-if __name__ == "__main__":
-    test()
+    for i in range(10):
+        obs, rew, done, info = env.step(env.action_space.sample())
+    env._is_done = True
+    env.reset()
+    for i in range(15):
+        obs, rew, done, info = env.step(env.action_space.sample())
+    env._is_done = True
+    env.reset()
