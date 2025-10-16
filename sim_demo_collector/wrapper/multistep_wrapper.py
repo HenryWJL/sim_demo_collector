19fbd9c523e222f456b8ff4ec7c52aaf62ba3e86
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-16 16:50:46 -0400

    Update multistep_wrapper.py and fix bugs

--- sim_demo_collector/wrapper/multistep_wrapper.py
+++ sim_demo_collector/wrapper/multistep_wrapper.py
@@ -1,8 +1,5 @@
-
 """
-MIT License
-
-Copyright (c) 2023 Columbia Artificial Intelligence and Robotics Lab
+Adapted from https://github.com/real-stanford/diffusion_policy/blob/main/diffusion_policy/gym_util/multistep_wrapper.py
 """
 import numpy as np
 import gymnasium as gym
@@ -76,95 +73,71 @@ def stack_last_n_obs(all_obs, n_steps):
         result[:start_idx] = result[start_idx]
     return result
 
-# Adapted from https://github.com/real-stanford/diffusion_policy/blob/main/diffusion_policy/gym_util/multistep_wrapper.py#L67
+
 class MultiStepWrapper(gym.Wrapper):
 
     def __init__(
         self,     
         env: gym.Env, 
-        obs_horizon: int, 
-        action_horizon: int, 
+        n_obs_steps: int, 
+        n_action_steps: int, 
         max_episode_steps: Optional[int] = None,
-        reward_agg_method: Literal['max', 'min', 'mean', 'sum'] = 'max',
-        enable_temporal_ensemble: Optional[bool] = True
+        reward_agg_method: Literal['max', 'min', 'mean', 'sum'] = 'max'
     ) -> None:
         super().__init__(env)
-        self._action_space = repeated_space(env.action_space, action_horizon)
-        self._observation_space = repeated_space(env.observation_space, obs_horizon)
-        self.obs = deque(maxlen=obs_horizon + 1)
-        if enable_temporal_ensemble:
-            assert max_episode_steps is not None
-            T = max_episode_steps
-            H = action_horizon
-            D = int(env.action_space.shape[0])
-            self.all_time_actions = np.zeros((T, T + H, D), dtype=np.float32)
-        self.reward = list()
-        self.done = list()
-        self.info = defaultdict(lambda : deque(maxlen=obs_horizon + 1))
-        self.global_step = 0
-        self.obs_horizon = obs_horizon
-        self.action_horizon = action_horizon
+        self.action_space = repeated_space(env.action_space, n_action_steps)
+        self.observation_space = repeated_space(env.observation_space, n_obs_steps)
         self.max_episode_steps = max_episode_steps
+        self.n_obs_steps = n_obs_steps
+        self.n_action_steps = n_action_steps
         self.reward_agg_method = reward_agg_method
-        self.enable_temporal_ensemble = enable_temporal_ensemble
 
-    def reset(self, seed: Optional[int] = None, options: Optional[Dict] = None):
-        obs = super().reset(seed=seed, options=options)
-        self.obs = deque([obs], maxlen=self.obs_horizon + 1)
-        if self.enable_temporal_ensemble:
-            self.all_time_actions = np.zeros_like(self.all_time_actions, dtype=np.float32)
+        self.obs = deque(maxlen=n_obs_steps+1)
         self.reward = list()
         self.done = list()
-        self.info = defaultdict(lambda : deque(maxlen=self.obs_horizon + 1))
-        self.global_step = 0
-        obs = self._get_obs(self.obs_horizon)
+        self.info = defaultdict(lambda : deque(maxlen=n_obs_steps+1))
+
+    def reset(self, *args, **kwargs) -> Dict:
+        obs = super().reset(*args, **kwargs)
+
+        self.obs = deque([obs], maxlen=self.n_obs_steps + 1)
+        self.reward = list()
+        self.done = list()
+        self.info = defaultdict(lambda : deque(maxlen=self.n_obs_steps + 1))
+
+        obs = self._get_obs(self.n_obs_steps)
         return obs
 
-    def step(self, actions: np.ndarray) -> Tuple:
+    def step(self, action: np.ndarray) -> Tuple:
         """
         Args:
-            actions (np.ndarray): (action_horizon, action_shape).
+            actions (np.ndarray): (n_action_steps, action_dim).
         """
-        if self.enable_temporal_ensemble:
-            t = self.global_step
-            H = self.action_horizon
-            self.all_time_actions[[t], t: t + H] = actions
-            current_actions = self.all_time_actions[:, t]
-            actions_populated = np.all(current_actions != 0, axis=1)
-            current_actions = current_actions[actions_populated]
-            exp_weights = np.exp(-0.01 * np.arange(len(current_actions)))
-            exp_weights = (exp_weights / exp_weights.sum())[..., np.newaxis]
-            action = (current_actions * exp_weights).sum(axis=0)
-            obs, reward, done, info = super().step(action)
-            self.obs.append(obs)
+        for act in action:
+            if len(self.done) > 0 and self.done[-1]:
+                # termination
+                break
+            observation, reward, done, info = super().step(act)
+
+            self.obs.append(observation)
             self.reward.append(reward)
-            if len(self.reward) >= self.max_episode_steps:
+            if (self.max_episode_steps is not None) \
+                and (len(self.reward) >= self.max_episode_steps):
+                # truncation
                 done = True
             self.done.append(done)
             self._add_info(info)
-            self.global_step += 1
-        else:
-            for action in actions:
-                if len(self.done) > 0 and self.done[-1]:
-                    break
-                obs, reward, done, info = super().step(action)
-                self.obs.append(obs)
-                self.reward.append(reward)
-                if (self.max_episode_steps is not None) and (len(self.reward) >= self.max_episode_steps):
-                    done = True
-                self.done.append(done)
-                self._add_info(info)
 
-        obs = self._get_obs(self.obs_horizon)
+        observation = self._get_obs(self.n_obs_steps)
         reward = aggregate(self.reward, self.reward_agg_method)
         done = aggregate(self.done, 'max')
-        info = dict_take_last_n(self.info, self.obs_horizon)
-        return obs, reward, done, info
+        info = dict_take_last_n(self.info, self.n_obs_steps)
+        return observation, reward, done, info
 
     def _get_obs(self, n_steps: Optional[int] = 1) -> Dict:
         """
         Returns:
-            obs (dict): each element with shape (obs_horizon, ...).
+            obs (dict): each element with shape (n_obs_steps, obs_shape).
         """
         assert(len(self.obs) > 0)
         if isinstance(self.observation_space, spaces.Box):
@@ -195,6 +168,3 @@ class MultiStepWrapper(gym.Wrapper):
         for k, v in self.info.items():
             result[k] = list(v)
         return result
-    
-    def close(self) -> None:
-        super().close()
