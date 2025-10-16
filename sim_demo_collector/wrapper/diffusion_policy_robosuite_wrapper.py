6608bfd98670c9ff8f3e5c67167d77b9ed846332
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-16 16:53:00 -0400

    Remove unnecessary files

--- sim_demo_collector/wrapper/diffusion_policy_robosuite_wrapper.py
+++
@@ -1,6 +0,0 @@
-# robosuite_key = key
-#             # Robomimic treats 'object-state' as 'object', and so does Diffusion Policy
-#             if key == 'object':
-#                 robosuite_key = 'object-state'
-#             assert robosuite_key in raw_obs, f"Key {key} not found in observation {raw_obs.keys()}"
-#             obs[key] = np.array(raw_obs[robosuite_key])
