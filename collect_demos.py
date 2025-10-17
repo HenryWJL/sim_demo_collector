d9398fccf59bd9b63c6856559a7be290a720a34c
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-17 10:56:28 -0400

    Add seed argument

--- collect_demos.py
+++ collect_demos.py
@@ -7,14 +7,15 @@ from pathlib import Path
 @click.option("-t", "--task", type=str, required=True, help="Simulation task.")
 @click.option("-r", "--runner", type=str, required=True, help="Policy runner.")
 @click.option("-c", "--checkpoint", type=str, default=None, help="Checkpoints.")
-def main(task, runner, checkpoint):
+@click.option("-s", "--seed", type=int, default=None, help="Initial seed.")
+def main(task, runner, checkpoint, seed):
     with hydra.initialize_config_dir(
         config_dir=str(Path(__file__).parent.joinpath("sim_demo_collector/config")),
         version_base=None
     ):
         cfg = hydra.compose(config_name="config", overrides=[f"task={task}"])
         runner = hydra.utils.instantiate(cfg.runner)
-        runner.run(checkpoint)
+        runner.run(checkpoint, seed)
 
 
 if __name__ == "__main__":
