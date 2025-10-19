ae09cbf34d3d5eeaaa4ad30394ca857e2d9a01ab
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-19 13:54:27 -0400

    Add image mask visualization

--- examples/visualize_image.py
+++ examples/visualize_image.py
@@ -1,14 +1,21 @@
 import cv2
 import zarr
+import numpy as np
 
 
 def visualize_image():
     with zarr.open("demos/lift_image/episodes.zarr", 'r') as f:
         images = f['agentview_image'][()]
+        masks = f['agentview_image_mask'][()]
         
     for _, image in enumerate(images):
         image = cv2.resize(cv2.cvtColor(image, cv2.COLOR_RGB2BGR), (512, 512))
         cv2.imshow("Image Sequence", image)
+        cv2.waitKey(20)
+
+    for _, mask in enumerate(masks):
+        mask = cv2.resize(mask.astype(np.uint8) * 255, (512, 512))
+        cv2.imshow("Image Mask Sequence", mask)
         cv2.waitKey(20)
 
 
