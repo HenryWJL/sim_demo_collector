5e0a3d7d27856419763d6795338c809c5b06d084
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-19 13:54:49 -0400

    Add point cloud mask visualization

--- examples/visualize_pc.py
+++ examples/visualize_pc.py
@@ -1,20 +1,30 @@
 import zarr
 import time
+import numpy as np
 import open3d as o3d
 
 
 def visualize_pc():
     with zarr.open("demos/lift_pc/episodes.zarr", 'r') as f:
         pc_seq = f['frontview_pc'][()]
+        pc_masks = f['frontview_pc_mask'][()]
     
     vis = o3d.visualization.Visualizer()
     vis.create_window(window_name="Point Cloud Sequence", width=960, height=540)
     pc = o3d.geometry.PointCloud()
     pc.points = o3d.utility.Vector3dVector(pc_seq[0])
+    colors = np.zeros_like(pc_seq[0])
+    colors[pc_masks[0] == 1] = [1, 0, 0]   # red = robot
+    colors[pc_masks[0] == 0] = [0, 1, 0]   # green = environment
+    pc.colors = o3d.utility.Vector3dVector(colors)
     vis.add_geometry(pc)
 
-    for _, raw_pc in enumerate(pc_seq):
+    for raw_pc, pc_mask in zip(pc_seq, pc_masks):
         pc.points = o3d.utility.Vector3dVector(raw_pc)
+        colors = np.zeros_like(raw_pc)
+        colors[pc_mask == 1] = [1, 0, 0]   # red = robot
+        colors[pc_mask == 0] = [0, 1, 0]   # green = environment
+        pc.colors = o3d.utility.Vector3dVector(colors)
         vis.update_geometry(pc)
         vis.poll_events()
         vis.update_renderer()
