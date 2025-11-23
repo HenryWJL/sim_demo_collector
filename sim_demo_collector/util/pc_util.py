28ebd2e09b551a09ec3e6fa1d54303dee50ec9d9
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-11-23 11:24:50 -0500

    Fix point cloud cropping bugs

--- sim_demo_collector/util/pc_util.py
+++ sim_demo_collector/util/pc_util.py
@@ -33,8 +33,8 @@ def depth2pc(
     bb_mask = np.ones_like(z, dtype=bool)
     if bounding_box is not None:
         bb_mask = (
-            (np.all(pc[:, :3] > bounding_box['lower_bound'], axis=1))
-            & (np.all(pc[:, :3] < bounding_box['upper_bound'], axis=1))
+            (np.all(pc[..., :3] > bounding_box['lower_bound'], axis=-1))
+            & (np.all(pc[..., :3] < bounding_box['upper_bound'], axis=-1))
         )
     pc = pc[bb_mask]
     if seg_mask is not None:
