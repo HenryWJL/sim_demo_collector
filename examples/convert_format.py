48f90ce7297bc1765419490bce948561ff2ea019
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-11-25 21:55:15 -0500

    Update convert_format.py

--- examples/convert_format.py
+++ examples/convert_format.py
@@ -5,8 +5,8 @@ import zarr
 import numpy as np
 from sim_demo_collector.util.transform_util import RotationTransformer
 
-with zarr.open("demos/square_pc/episodes.zarr", 'r') as f:
-    actions = f['action'][()]
+with zarr.open("demos/robosuite_can_pc.zarr", 'r') as f:
+    actions = f['data/action'][()]
 
 transformer = RotationTransformer()
 trans = actions[:, :3]
@@ -16,6 +16,6 @@ rot = transformer.forward(rot)
 actions = np.concatenate([trans, rot, gripper], axis=1)
 print("Action shape: ", actions.shape)
 
-with zarr.open("demos/square_pc/episodes.zarr", 'a') as f:
-    del f["action"]
-    f.create_dataset("action", data=actions, chunks=True)
+with zarr.open("demos/robosuite_can_pc.zarr", 'a') as f:
+    del f["data/action"]
+    f.create_dataset("data/action", data=actions, chunks=True)
