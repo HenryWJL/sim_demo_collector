7da4b26d0e52aee79eba241d58ad11b7df9f9742
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-04-11 10:15:12 -0400

    Delete redundant code

--- sim_demo_collector/runner/diffusion_policy_runner.py
+++ sim_demo_collector/runner/diffusion_policy_runner.py
@@ -6,9 +6,7 @@ import numpy as np
 import gymnasium as gym
 import torchvision.transforms.functional as F
 from typing import Optional, Dict
-from diffusion_policy.policy.base_image_policy import BaseImagePolicy
 from diffusion_policy.workspace.base_workspace import BaseWorkspace
-from sim_demo_collector.util.transform_util import RotationTransformer
 
 
 class DiffusionPolicyRunner:
@@ -16,20 +14,14 @@ class DiffusionPolicyRunner:
     def __init__(
         self,
         env: gym.Env,
-        policy: BaseImagePolicy,
-        shape_meta: Dict,
         max_episode_steps: Optional[int] = None,
         num_episodes: Optional[int] = 50,
         device: Optional[str] = "cpu"
     ) -> None:
         self.env = env
-        self.policy = policy
-        self.shape_meta = shape_meta
         self.max_episode_steps = max_episode_steps
         self.num_episodes = num_episodes
         self.device = torch.device(device)
-        self.policy.to(self.device)
-        self.policy.eval()
     
     def _extract_obs(self, raw_obs: Dict) -> Dict:
         """
@@ -46,26 +38,26 @@ class DiffusionPolicyRunner:
                 obs[key] = torch.from_numpy(raw_obs[key]).float().unsqueeze(0).to(self.device)
         return obs
     
-    def run(self, checkpoint: Optional[str] = None, init_seed: Optional[int] = None) -> None:
-        if checkpoint is not None:
-            payload = torch.load(open(checkpoint, 'rb'), pickle_module=dill)
-            cfg = payload['cfg']
-            cls = hydra.utils.get_class(cfg._target_)
-            workspace: BaseWorkspace = cls(cfg)
-            workspace.load_payload(payload, exclude_keys=None, include_keys=None)
-            # Get policy from workspace
-            self.policy = workspace.model
-            if cfg.training.use_ema:
-                self.policy = workspace.ema_model
-            self.policy.to(self.device)
-            self.policy.eval()
+    def run(self, checkpoint: str, init_seed: Optional[int] = None) -> None:
+        # Load checkpoints
+        payload = torch.load(open(checkpoint, 'rb'), pickle_module=dill)
+        cfg = payload['cfg']
+        # Get shape information
+        self.shape_meta = cfg.shape_meta
+        # Get policy from workspace
+        cls = hydra.utils.get_class(cfg._target_)
+        workspace: BaseWorkspace = cls(cfg)
+        workspace.load_payload(payload, exclude_keys=None, include_keys=None)
+        if cfg.training.use_ema:
+            policy = workspace.ema_model
+        else:
+            policy = workspace.model
+        policy.to(self.device)
+        policy.eval()
 
-        seed_increment = 0
+        seed = init_seed
         success_cnt = 0
         while True:
-            seed = None
-            if init_seed is not None:
-                seed = init_seed + seed_increment
             obs = self.env.reset(seed=seed)
             self.env.render()
             pbar = tqdm.tqdm(
@@ -78,7 +70,7 @@ class DiffusionPolicyRunner:
             while not done:
                 obs = self._extract_obs(obs)
                 with torch.no_grad():
-                    action = self.policy.predict_action(obs)['action']
+                    action = policy.predict_action(obs)['action']
                 action = action.detach().cpu().squeeze(0).numpy()
                 obs, _, done, info = self.env.step(action)
                 self.env.render()
@@ -88,8 +80,9 @@ class DiffusionPolicyRunner:
                     success_cnt += 1
                 pbar.update(action.shape[1])
             pbar.close()
-            seed_increment += 1
             if success_cnt == self.num_episodes:
                 break
+            if seed is not None:
+                seed += 1
         # Clear out data buffer
         self.env.reset()
